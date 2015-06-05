b9cd9531c25037d6726309d213c93628433eb87b
leafar13 <rafaportuguez@gmail.com>
2015-06-04 22:52:12 -0600

    Se agregó una actividad
    La actividad tiene una funcionalidad demasiado elaborada que el que la
    vea le va dar un derrame.

--- Cupones/app/src/main/java/com/apptions/cupones/InicioSesion.java
+++ Cupones/app/src/main/java/com/apptions/cupones/InicioSesion.java
@@ -6,24 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class InicioSesion extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-        ///patito
-        // otro patito
+        setContentView(R.layout.activity_inicio_sesion);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_inicio_sesion, menu);
         return true;
     }
 
